ea7ba445a1934afe50111abc5c820b769f659a39
Prabhat Ojha <prabhat.ojha@autodesk.com>
2016-10-13 12:41:16 +0530

    init commit

---
+++ JSearchDemo/jsearch.ts
@@ -0,0 +1,1 @@
+export {JSearch} from './src/jsearch.service';
---
+++ JSearchDemo/src/jsearch.service.ts
@@ -0,0 +1,15 @@
+export class JSearch {
+    object: any;
+
+    constructor(object: any) {
+        this.object = object;
+    }
+
+    getObjectByKey() {
+        return "get object by key";
+    }
+
+    getObjectByValue() {
+        return "get object by value";
+    }
+}
